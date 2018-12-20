3cb824d722be45b227b041419435aa9c029d4eaf
Aadil Mehraj <aadilMehraj76@gmail.com>
2018-12-20 15:14:11 +0530

    Add SplashActivity and MediatorLiveData

--- app/src/main/java/com/aadilmehraj/android/mvvmarch/viewmodel/MainViewModel.java
+++ app/src/main/java/com/aadilmehraj/android/mvvmarch/viewmodel/MainViewModel.java
@@ -2,34 +2,66 @@ package com.aadilmehraj.android.mvvmarch.viewmodel;
 
 import android.app.Application;
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
+import android.support.annotation.Nullable;
+import com.aadilmehraj.android.mvvmarch.service.model.Category;
 import com.aadilmehraj.android.mvvmarch.service.model.Model;
 import com.aadilmehraj.android.mvvmarch.service.repository.MainRepository;
 import com.google.firebase.database.FirebaseDatabase;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainViewModel extends ViewModel {
 
     private Application mAppContext;
     private MainRepository mMainRepository;
 
-    private MutableLiveData<Model> mItemListLive;
+    private MediatorLiveData<Model> mItemListLive = new MediatorLiveData<>();
+    private MediatorLiveData<List<Category>> mCategoryListLive = new MediatorLiveData<>();
     private LiveData<String> mErrorLive;
+
+    private ArrayList<Category> mCategories;
 
     public MainViewModel(Application appContext, MainRepository mainRepository) {
         mAppContext = appContext;
         mMainRepository = mainRepository;
 
-        fetchData();
         mErrorLive = mainRepository.getErrorLive();
     }
 
     /**
      * Fetches the data from the {@link FirebaseDatabase}
-     *
      */
-    private void fetchData() {
-        mItemListLive = (MutableLiveData<Model>) mMainRepository.queryFirebase();
+    public void fetchData() {
+
+        final LiveData<Model> modelLiveData = mMainRepository.queryFirebase();
+        LiveData<List<Category>> categoriesLiveData = mMainRepository.getCategoryList();
+
+        mItemListLive.addSource(modelLiveData, new Observer<Model>() {
+            @Override
+            public void onChanged(@Nullable Model model) {
+                mItemListLive.removeSource(modelLiveData);
+                mItemListLive.setValue(model);
+            }
+        });
+
+        mCategoryListLive.addSource(categoriesLiveData,
+            new Observer<List<Category>>() {
+                @Override
+                public void onChanged(@Nullable List<Category> categories) {
+                    mCategoryListLive.removeSource(mCategoryListLive);
+                    mCategories = (ArrayList<Category>) categories;
+                    mCategoryListLive.setValue(categories);
+                }
+            });
+    }
+
+
+    public void setCategoryList(List<Category> categories) {
+        mCategories = (ArrayList<Category>) categories;
+        mCategoryListLive.setValue(categories);
     }
 
     public LiveData<Model> getItemListLive() {
@@ -38,5 +70,13 @@ public class MainViewModel extends ViewModel {
 
     public LiveData<String> getErrorLive() {
         return mErrorLive;
+    }
+
+    public LiveData<List<Category>> getCategoryListLive() {
+        return mCategoryListLive;
+    }
+
+    public ArrayList<Category> getCategories() {
+        return mCategories;
     }
 }
